5ef3de3aeb0e2aba17f80cbdfdd75d5be019007e
mmk119 <mohammad15kassem@gmail.com>
2026-06-14 17:47:02 +0300

    Add GitHub Pages deployment
    Configure Vite base path, switch to HashRouter, and add GitHub Actions workflow for automatic deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -6,12 +6,12 @@ import './styles/index.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
